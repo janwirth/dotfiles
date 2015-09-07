081479b16b58ff9902aa72efea953333f0a38291
Jan Wirth <jan_wirth@visual4.de>
2015-09-07 15:41:38 +0200

    Automate neobundle installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,11 @@
+BUNDLE_DIR=~/.vim/bundle
+INSTALL_DIR=$BUNDLE_DIR/neobundle.vim
+
 touch ~/.vimrc
 ln -s ~/.vimrc ~/.nvimrc
+rm 
+ln -s ~/.vimrc ~/.nvimrc
+ln -s ~/.vim ~/.nvim
 echo 'source dotfiles/.nvimrc' >> ~/.vimrc
+git clone https://github.com/Shougo/neobundle.vim $INSTALL_DIR
+
