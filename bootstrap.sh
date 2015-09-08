20e90a0dab2d8b5c50bbddc651af1db7bf91ed9c
Jan Wirth <jan_wirth@visual4.de>
2015-09-08 12:48:11 +0200

    Fix paths; create symlinks

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,9 +3,9 @@ INSTALL_DIR=$BUNDLE_DIR/neobundle.vim
 
 touch ~/.vimrc
 ln -s ~/.vimrc ~/.nvimrc
-rm 
-ln -s ~/.vimrc ~/.nvimrc
+rm ~/.viminfo
+ln -s ~/.viminfo ~/.nviminfo
 ln -s ~/.vim ~/.nvim
-echo 'source dotfiles/.nvimrc' >> ~/.vimrc
+echo 'source ~/dotfiles/.vimrc' >> ~/.vimrc
 git clone https://github.com/Shougo/neobundle.vim $INSTALL_DIR
 
